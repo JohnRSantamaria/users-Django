58833abb7ddd6eac4b962b45348a7c86c696c228
JohnRSantamaria <santamariajohnd@gmail.com>
2024-04-14 11:38:57 -0500

    feat: cambios en los modelos, sin importancia solo migraciones

--- usuarios/applications/users/models.py
+++ usuarios/applications/users/models.py
@@ -1,31 +1,36 @@
 from django.db import models
 
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
 # Create your models here.
 
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin):
 
-    GENDER_CHIOSES = (
+    GENDER_CHOISES = (
         ('M', 'Masculino'),
         ('F', 'Femenino'),
         ('X', 'Otros'),
     )
 
-    username = models.CharField('usuarios', max_length=10, unique=True)
+    username = models.CharField(
+        'Nombre de adminstrador',
+        max_length=10,
+        unique=True
+    )
     email = models.EmailField(max_length=254)
     nombres = models.CharField(max_length=50)
     apellidos = models.CharField(max_length=50)
     genero = models.CharField(
         max_length=1,
-        choices=GENDER_CHIOSES,
+        choices=GENDER_CHOISES,
         blank=True
     )
+
+    USERNAME_FIELD = 'username'
 
     def get_short_name(self):
         return self.username
 
     def get_full_name(self):
         return f"{self.nombres} {self.apellidos}"
-    
